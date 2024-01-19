f98c58a62cb4c6226baec163c662601aacc69a6f
JoaquinDarquier <joacodarquier99@gmail.com>
2024-01-18 21:14:10 -0300

    Creacion de modelos (Arquero, Defensor, Mediocampista, Delantero) y se corrieron las migraciones.

--- proyecto/app/models.py
+++ proyecto/app/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+class Arquero (models.Model):
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    pie_habil = models.CharField(max_length=20)
+    cant_clubes_anteriores = models.IntegerField()
+    telefono = models.IntegerField()
+    estatura = models.FloatField()
+    email = models.EmailField()
+
+class Defensor (models.Model):
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    pie_habil = models.CharField(max_length=20)
+    cant_clubes_anteriores = models.IntegerField()
+    telefono = models.IntegerField()
+    estatura = models.FloatField()
+    email = models.EmailField()
+
+class Mediocampista (models.Model):
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    pie_habil = models.CharField(max_length=20)
+    cant_clubes_anteriores = models.IntegerField()
+    telefono = models.IntegerField()
+    estatura = models.FloatField()
+    email = models.EmailField()
+
+class Delantero (models.Model):
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    pie_habil = models.CharField(max_length=20)
+    cant_clubes_anteriores = models.IntegerField()
+    telefono = models.IntegerField()
+    estatura = models.FloatField()
+    email = models.EmailField()
+
