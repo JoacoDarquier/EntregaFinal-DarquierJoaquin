67578c1545709816e051128e3af62c6b155ceaa4
JoaquinDarquier <joacodarquier99@gmail.com>
2024-01-27 13:53:07 -0300

    Se agregaron los modelos al admin. Y se cambio la vista de los jugadores de cuando ya estan cargados

--- proyecto/app/admin.py
+++ proyecto/app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from app.models import Arquero, Defensor, Mediocampista, Delantero
+
+
+admin.site.register(Arquero)
+admin.site.register(Defensor)
+admin.site.register(Mediocampista)
+admin.site.register(Delantero)
